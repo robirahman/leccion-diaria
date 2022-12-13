88902ba4680a180411e8ee722d1f6517d44767d8
Robi Rahman <robirahman94@gmail.com>
2022-12-13 16:07:15 -0500

    feat: conjugate regular verbs
    Implement function to conjugate regular verbs.

--- daily_quiz.py
+++ daily_quiz.py
@@ -1,23 +1,35 @@
 import random
 from util import get_verb
 
+
 def daily_quiz():
     """Quiz the user on the conjugation of a verb."""
     # Select a tense
-    tenses = ["Presente", "Preterito", "Imperfecto", "Futuro"]
+    tenses = ["present", "preterite", "imperfect", "future"]
     tense = random.choice(tenses)
 
     # Select a verb
     verb = get_verb()
 
+    # Select a person
+    persons = [
+        "first person",  # yo
+        "second person singular informal",  # tú
+        "third person singular or second person singular formal",  # él/ella/usted
+        "first person plural",  # nosotros
+        "second person plural informal",  # vosotros
+        "third person plural or second person plural formal"  # ellos/ellas/ustedes
+    ]
+    person = random.choice(persons)
+
     # Get the verb stem and ending
     stem, ending = verb["English"][:-2], verb["English"][-2:]
 
-    # Get the conjugation of the verb in the tense
+    # Get the conjugation of the verb in the tense and person
     raise NotImplementedError
 
-    # Ask the user to conjugate the verb in the tense
-    response = input(f"Conjugate {verb['Español']} in the {tense} tense: ")
+    # Ask the user to conjugate the verb in the tense and person
+    response = input(f"Conjugate {verb['English']} in the {tense} tense and {person}: ")
 
     # Check if the user's response is correct
     raise NotImplementedError
--- util.py
+++ util.py
@@ -6,3 +6,119 @@ def get_verb() -> str:
     verb_list = pd.read_csv("verbos regulares.csv", names=["Español", "English"])
     verb = verb_list.sample(1)
     return verb
+
+
+def conjugate(verb: str, tense: str, person: str) -> str:
+    """Conjugate a verb in a tense and person."""
+
+    # split verb into stem and ending
+    stem, ending = verb[:-2], verb[-2:]
+
+    endings_dict = {
+        "ar": {
+            "present": {
+                "yo": "o",
+                "tú": "as",
+                "él/ella/usted": "a",
+                "nosotros": "amos",
+                "vosotros": "áis",
+                "ellos/ellas/ustedes": "an"
+            },
+            "preterite": {
+                "yo": "é",
+                "tú": "aste",
+                "él/ella/usted": "ó",
+                "nosotros": "amos",
+                "vosotros": "asteis",
+                "ellos/ellas/ustedes": "aron"
+            },
+            "imperfect": {
+                "yo": "aba",
+                "tú": "abas",
+                "él/ella/usted": "aba",
+                "nosotros": "ábamos",
+                "vosotros": "abais",
+                "ellos/ellas/ustedes": "aban"
+            },
+            "future": {
+                "yo": "aré",
+                "tú": "arás",
+                "él/ella/usted": "ará",
+                "nosotros": "aremos",
+                "vosotros": "aréis",
+                "ellos/ellas/ustedes": "arán"
+            }
+        },
+        "er": {
+            "present": {
+                "yo": "o",
+                "tú": "es",
+                "él/ella/usted": "e",
+                "nosotros": "emos",
+                "vosotros": "éis",
+                "ellos/ellas/ustedes": "en"
+            },
+            "preterite": {
+                "yo": "í",
+                "tú": "iste",
+                "él/ella/usted": "ió",
+                "nosotros": "imos",
+                "vosotros": "isteis",
+                "ellos/ellas/ustedes": "ieron"
+            },
+            "imperfect": {
+                "yo": "ía",
+                "tú": "ías",
+                "él/ella/usted": "ía",
+                "nosotros": "íamos",
+                "vosotros": "íais",
+                "ellos/ellas/ustedes": "ían"
+            },
+            "future": {
+                "yo": "eré",
+                "tú": "erás",
+                "él/ella/usted": "erá",
+                "nosotros": "eremos",
+                "vosotros": "eréis",
+                "ellos/ellas/ustedes": "erán"
+            }
+        },
+        "ir": {
+            "present": {
+                "yo": "o",
+                "tú": "es",
+                "él/ella/usted": "e",
+                "nosotros": "imos",
+                "vosotros": "ís",
+                "ellos/ellas/ustedes": "en"
+            },
+            "preterite": {
+                "yo": "í",
+                "tú": "iste",
+                "él/ella/usted": "ió",
+                "nosotros": "imos",
+                "vosotros": "isteis",
+                "ellos/ellas/ustedes": "ieron"
+            },
+            "imperfect": {
+                "yo": "ía",
+                "tú": "ías",
+                "él/ella/usted": "ía",
+                "nosotros": "íamos",
+                "vosotros": "íais",
+                "ellos/ellas/ustedes": "ían"
+            },
+            "future": {
+                "yo": "iré",
+                "tú": "irás",
+                "él/ella/usted": "irá",
+                "nosotros": "iremos",
+                "vosotros": "iréis",
+                "ellos/ellas/ustedes": "irán"
+            }
+        }
+    }
+
+    conjugated_ending = endings_dict[ending][tense][person]
+
+    return stem + conjugated_ending
